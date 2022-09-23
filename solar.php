cb879513e8c98d1bab1d0b40279132280380b0ea
drzh <zhangheqd@gmail.com>
2022-09-22 19:35:44 -0500

    modified:   solar.php

--- solar.php
+++ solar.php
@@ -7,18 +7,20 @@
 <tr style='vertical-align:top'><td>
 <?php
 $rsl = '256';
+$rsl_link = '1024';
+$imgurlbase = 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_';
 $imgurl = [
-    'HMI Continuum' => 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_' . $rsl . '_HMIIC.jpg',
-    'AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_' . $rsl . '_0304.jpg',
-    'AIA 193 &Aring / 1 millon K / Corona / Flare plasma' => 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_' . $rsl . '_0193.jpg',
-    'AIA 131 &Aring / 10 millon K / Flaring region' => 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_' . $rsl . '_0131.jpg',
+    'HMI Continuum' => 'HMIIC.jpg',
+    'AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => '0304.jpg',
+    'AIA 193 &Aring / 1 millon K / Corona / Flare plasma' => '0193.jpg',
+    'AIA 131 &Aring / 10 millon K / Flaring region' => '0131.jpg',
 ];
 
 $ran = rand(1,1000000);
 
 foreach ($imgurl as $title => $url) {
     echo "<h2>$title</h2>";
-    echo "<img src=$url?=$ran alt='$title' />";
+    echo "<a href='${imgurlbase}${rsl_link}_$url?$=$ran' target='_blank'><img src='${imgurlbase}${rsl}_$url?=$ran' alt='$title' /></a>";
 }
 
 ?>
