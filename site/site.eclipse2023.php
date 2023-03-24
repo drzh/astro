70005e7b6684b85407d74a6859f0c3c221e0d1ec
celaeno <zhangheqd@gmail.com>
2023-03-23 21:53:13 -0500

    new file:   goes/site.pos 	new file:   planet/jupiter/jupiter.grs_transit.2023.UTC.format 	new file:   site/site.eclipse2023.php

---
+++ site/site.eclipse2023.php
@@ -0,0 +1,33 @@
+<?php
+$pos = [
+    [
+        'Pleasanton',
+        28.940,
+        -98.511,
+        'http://www.cleardarksky.com/c/ClvrsHObTXcsk.gif',
+        'http://www.cleardarksky.com/c/ClvrsHObTXkey.html',
+        'corpuschristi',
+        'http://forecast.weather.gov/meteograms/Plotter.php?lat=28.94&lon=-98.511&wfo=EWX&zcode=TXZ220&gset=18&gdiff=3&unit=0&tinfo=CY6&ahour=0&pcmd=00000111000000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6',
+        'TX'
+    ],
+    [
+        'Corpus Christi',
+        27.801,
+        -97.454,
+        'http://www.cleardarksky.com/c/CrpsChrTXcsk.gif',
+        'http://www.cleardarksky.com/c/CrpsChrTXkey.html',
+        'corpuschristi',
+        'http://forecast.weather.gov/meteograms/Plotter.php?lat=27.801&lon=-97.454&wfo=CRP&zcode=GMZ232&gset=18&gdiff=6&unit=0&tinfo=CY6&ahour=0&pcmd=00000111000000000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6',
+        'TX'
+    ],
+    [
+        'Cloud coverage along the central line',
+        365,
+        365,
+        'https://eclipse.aas.org/sites/eclipse.aas.org/files/October-Mean-Cloud-Fraction.png',
+        '',
+        'histcloud',
+        '',
+        'History Cloud Coverage'
+    ],
+];
