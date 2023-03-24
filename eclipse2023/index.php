b1410aecffb0c2f462f778375ee613602d15f404
celaeno <zhangheqd@gmail.com>
2023-03-23 21:50:32 -0500

    new file:   eclipse2023/index.php

---
+++ eclipse2023/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php include("../head.php") ?>
+<body>
+  <?php
+  include('../site/site.eclipse2023.php');
+  include('../menu.php');
+  include('../weather.php');
+  ?>
+</body>
+</html>
