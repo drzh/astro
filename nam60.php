566676d3adb76bac636633ed73165778efd2f446
celaeno <zhangheqd@gmail.com>
2023-10-12 10:33:49 -0500

    new file:   eclipse2023/nam60.php 	new file:   eclipse2023/nam84.php 	new file:   nam60.php 	new file:   nam84.php

---
+++ nam60.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<?php include("head.php") ?>
+
+<body>
+  <?php
+  $datadir = '/home/celaeno/web/astro/nam';
+  $f1 = $datadir . '/all.skycover.60hr.UTC.format';
+  $f2 = '';
+  $f3 = '';
+  
+  require 'site/site.php';
+  require 'menu.php';
+  include 'plot_weather.php';
+  include('tail.php');
+  ?>
+</body>
+
+</html>
