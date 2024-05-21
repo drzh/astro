4739309d8467164bf0edbccc3444ec6dfdf606b0
celaeno <zhangheqd@gmail.com>
2024-05-21 16:46:35 -0500

    modified:   comet/get_comet_list.sh 	modified:   link.php 	modified:   satellite/satslist.csv

--- comet/get_comet_list.sh
+++ comet/get_comet_list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/celaeno/web/astro/table/
+cd /home/celaeno/web/astro/comet/
 wget "https://cobs.si/api/comet_list.api?is-observed=true" -O cobs.commet.list.observed.json
 python3 ~/script/hobby/astro/convert_json_to_table.py -i cobs.commet.list.observed.json -o cobs.commet.list.observed.json.txt -k objects
-cat <(head -n1 cobs.commet.list.observed.json.txt) <(cat cobs.commet.list.observed.json.txt | sed '1d' | awk -F'\t' '{if($7!="" && $7<=16)print}' | sort -t$'\t' -k7,7g) > tmp
+cat <(head -n1 cobs.commet.list.observed.json.txt) <(cat cobs.commet.list.observed.json.txt | sed '1d' | awk -F'\t' '{if($8!="" && $8<=16)print}' | sort -t$'\t' -k8,8g) > tmp
 mv tmp cobs.commet.list.observed.json.txt
