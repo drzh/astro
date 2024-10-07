d112f5fb6901aae3b6fb9850b343016fea43fd32
celaeno <zhangheqd@gmail.com>
2024-10-07 12:51:21 -0500

    modified:   satellite/satslist.csv 	modified:   solar.php

--- solar.php
+++ solar.php
@@ -8,19 +8,21 @@
 <?php
 $rsl = '256';
 $rsl_link = '1024';
-$imgurlbase = 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_';
+$imgurlbase1 = 'http://sdo.gsfc.nasa.gov/assets/img/latest/latest_';
 $imgurl = [
-    'HMI Continuum' => 'HMIIC.jpg',
-    'AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => '0304.jpg',
-    'AIA 193 &Aring / 1 millon K / Corona / Flare plasma' => '0193.jpg',
-    'AIA 131 &Aring / 10 millon K / Flaring region' => '0131.jpg',
+    'HMI Continuum' => [$imgurlbase1 . $rsl . '_HMIIC.jpg', $imgurlbase1 . $rsl_link . '_HMIIC.jpg'],
+    'AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => [$imgurlbase1 . $rsl . '_0304.jpg', $imgurlbase1 . $rsl_link . '_0304.jpg'],
+    #'AIA 193 &Aring / 1 millon K / Corona / Flare plasma' => [$imgurlbase1 . $rsl . '_0193.jpg', $imgurlbase1 . $rsl_link . '_0193.jpg'],
+    #'AIA 131 &Aring / 10 millon K / Flaring region' => [$imgurlbase1 . $rsl . '_0131.jpg', $imgurlbase1 . $rsl_link . '_0131.jpg'],
+    'LASCO C2' => ['https://soho.nascom.nasa.gov/data/realtime/c2/1024/latest.jpg', 'https://soho.nascom.nasa.gov/data/realtime/c2/1024/latest.jpg'],
+    'LASCO C3' => ['https://soho.nascom.nasa.gov/data/realtime/c3/1024/latest.jpg', 'https://soho.nascom.nasa.gov/data/realtime/c3/1024/latest.jpg'],
 ];
 
 $ran = rand(1,1000000);
 
 foreach ($imgurl as $title => $url) {
     echo "<h2>$title</h2>";
-    echo "<a href='${imgurlbase}${rsl_link}_$url?$=$ran' target='_blank'><img src='${imgurlbase}${rsl}_$url?=$ran' alt='$title' /></a>";
+    echo "<a href='$url[1]?$=$ran' target='_blank'><img width='256' height='256' src='$url[0]?=$ran' alt='$title' /></a>";
 }
 
 ?>
