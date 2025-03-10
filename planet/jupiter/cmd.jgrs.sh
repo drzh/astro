ac1286b15a23f8f670d4a18914f8ac6523917328
celaeno <zhangheqd@gmail.com>
2025-03-10 04:41:58 -0500

    modified:   planet/jupiter/calc_jgrs.pl 	modified:   planet/jupiter/cmd.jgrs.sh 	modified:   satellite/satslist.csv

--- planet/jupiter/cmd.jgrs.sh
+++ planet/jupiter/cmd.jgrs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./getdate.sh 20240509 20250101 | ./calc_jgrs.pl | sort | uniq -w11 | awk '{print "Jupiter\tGRS Tra\t"$0}' > jupiter.grs_transit.2024.UTC.format
+./getdate.sh 20250101 20260101 | ./calc_jgrs.pl | sort | uniq -w11 | awk '{print "Jupiter\tGRS Tra\t"$0}' > jupiter.grs_transit.2025.UTC.format
