629505b5c4c2271a961e8a19075ecb3faea45161
celaeno <zhangheqd@gmail.com>
2026-01-10 12:20:25 -0600

    modified:   aurora.php

--- aurora.php
+++ aurora.php
@@ -7,14 +7,14 @@
 <table>
 <tr style='vertical-align:top'><td>
 <?php
-echo "<h2>Aurora Forecast - 3 Days</h2>";
+echo "<h2>NOAA Aurora Forecast - 3 Days</h2>";
 $filename = 'sun/aurora.3day.tsv';
 display_table_from_tsv($filename, 1);
 echo "<br/>";
 
 $imgurl = [
-    'NOAA Aurora Forecast - 3 Days' => ['https://spaceweather.gfz.de/fileadmin/SW-Monitor/kp_swift_ensemble_LAST.png', 'https://spaceweather.gfz.de/fileadmin/SW-Monitor/kp_swift_ensemble_LAST.png', 660, 352],
-    'GFZ Aurora Forecast - Tonight' => ['https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png', 'https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png', 512, 512],
+    'GFZ Aurora Forecast - 3 Days' => ['https://spaceweather.gfz.de/fileadmin/SW-Monitor/kp_swift_ensemble_LAST.png', 'https://spaceweather.gfz.de/fileadmin/SW-Monitor/kp_swift_ensemble_LAST.png', 660, 352],
+    'Aurora Forecast - Tonight' => ['https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png', 'https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png', 512, 512],
     'Aurora Forecast - 30 mins' => ['https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg', 'https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg', 512, 512],
     'GOES19 CCOR-1' => ['https://services.swpc.noaa.gov/images/animations/ccor1/latest.jpg', 'https://services.swpc.noaa.gov/images/animations/ccor1/latest.jpg', 512, 512],
     'LASCO C2' => ['https://soho.nascom.nasa.gov/data/realtime/c2/512/latest.jpg', 'https://soho.nascom.nasa.gov/data/realtime/c2/1024/latest.jpg', 512, 512],
