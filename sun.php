6882cbac8e0268665fe3e5c4ead4142c5b686648
celaeno <zhangheqd@gmail.com>
2026-01-24 16:02:39 -0600

    modified:   sun.php

--- sun.php
+++ sun.php
@@ -6,8 +6,24 @@
 <table>
 <tr style='vertical-align:top'><td>
 <?php
+# Get the 'intensity_max' from 'AIAsynoptic0304.full.txt'
+$fi = 'sun/AIAsynoptic0304.full.txt';
+$intensity_max = null;
+if (file_exists($fi)) {
+    $lines = file($fi);
+    foreach ($lines as $line) {
+        if (strpos($line, 'intensity_max') !== false) {
+            $parts = preg_split('/\s+/', trim($line));
+            if (count($parts) >= 2) {
+                $intensity_max = $parts[1];
+            }
+            break;
+        }
+    }
+}
+
 $imgurl = [
-    'AIA 304 | <a href="/sun/AIAsynoptic0304.full.txt">Data</a>' => ['sun/AIAsynoptic0304.full.png', 'sun/AIAsynoptic0304.full.png', 256, 256],
+    'AIA 304 | <a href="/sun/AIAsynoptic0304.full.txt">Max: ' . $intensity_max . '</a>' => ['sun/AIAsynoptic0304.full.png', 'sun/AIAsynoptic0304.full.png', 256, 256],
     'GOES-19 SUVI - AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => ['https://services.swpc.noaa.gov/images/animations/suvi/primary/304/latest.png', 'https://services.swpc.noaa.gov/images/animations/suvi/primary/304/latest.png', 256, 256],
     'HMI Continuum' => ['https://soho.nascom.nasa.gov/data/realtime/hmi_igr/512/latest.jpg', 'https://soho.nascom.nasa.gov/data/realtime/hmi_igr/1024/latest.jpg', 256, 256],
     #'AIA 304 &Aring / 50,000 K / Transition region / Chromosphere' => ['https://soho.nascom.nasa.gov/data/realtime/eit_304/512/latest.jpg', 'https://soho.nascom.nasa.gov/data/realtime/eit_304/1024/latest.jpg', 256, 256],
